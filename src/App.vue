baa93577c9cd0c3c97de670e545d99433fc4624c
班班 <@qq.com>
2020-02-07 11:55:07 +0800

    项目起步 - 整合路由

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  
+  <router-link to="info">路由</router-link>
 </template>
 
 <script>
---
+++ src/router/index.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+const router = new VueRouter({
+    //路由规则
+    routes:[]
+})
+export default router
