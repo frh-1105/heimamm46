9ee08deae78615fb1608dd54c0069404110203fe
班班 <@qq.com>
2020-02-08 11:03:19 +0800

    登录模块 - 整体布局并绑定表单

--- src/view/login/login.vue
+++ src/view/login/login.vue
@@ -3,12 +3,44 @@
   <div class="login-container">
     <!-- 左侧盒子 -->
     <div class="left-box">
+      <!-- 标题区域 -->
       <div class="title-box">
         <img class="login-logo" src="../../assets/login_logo.png" alt />
         <span class="title">黑马面面</span>
         <span class="line"></span>
         <span class="sub-title">用户登录</span>
       </div>
+      <!-- 登录表单 -->
+      <el-form ref="form" :model="loginForm" label-width="43px" class="login-form">
+        <el-form-item>
+          <el-input placeholder="请输入手机号码" prefix-icon="el-icon-user" v-model="loginForm.phone"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input placeholder="请输入密码" prefix-icon="el-icon-lock" v-model="loginForm.password"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-row>
+            <el-col :span="17">
+              <el-input placeholder="请输入验证码" prefix-icon="el-icon-key" v-model="loginForm.loginCode"></el-input>
+            </el-col>
+            <el-col :span="7">
+              <img class="captcha" src alt />
+            </el-col>
+          </el-row>
+        </el-form-item>
+        <el-form-item>
+          <el-checkbox v-model="loginForm.isChecked" label="我已同意并阅读">
+            <span>我已同意并阅读</span>
+            <el-link type="primary">用户协议</el-link>
+            <span>和</span>
+            <el-link type="primary">隐私条款</el-link>
+          </el-checkbox>
+        </el-form-item>
+        <el-form-item class="btn-box">
+          <el-button type="primary">登录</el-button>
+          <el-button type="primary">注册</el-button>
+        </el-form-item>
+      </el-form>
     </div>
     <!-- 右侧图片 -->
     <img src="../../assets/login_banner_ele.png" alt />
@@ -18,7 +50,17 @@
 <script>
 export default {
   //组件名字
-  name: "login"
+  name: "login",
+  data() {
+    return {
+      loginForm: {
+        phone: "",
+        password: "",
+        loginCode: "",
+        isChecked:false
+      }
+    };
+  }
 };
 </script>
 
@@ -59,6 +101,19 @@ export default {
       }
       .sub-title {
         font-size: 22px;
+      }
+    }
+    .login-form {
+      margin: 29px 41px 0 0;
+      .captcha {
+        width: 100%;
+      }
+      .btn-box {
+        .el-button {
+          width: 100%;
+          margin-left: 0;
+          margin-bottom: 28px;
+        }
       }
     }
   }
