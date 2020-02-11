37483094c04c65c55f0289086b9d61d6539e2d25
班班 <@qq.com>
2020-02-11 15:35:51 +0800

    登录模块 - 登录接口抽取

---
+++ src/api/login.js
@@ -0,0 +1,14 @@
+import axios from 'axios'
+const loginRequest = axios.create({
+    baseURL:process.env.VUE_APP_URL,
+    // ajax跨域请求不会携带cookie，需要人为设置
+    withCredentials: true,
+})
+//用户登录接口
+export function login(data) {
+    return loginRequest({
+        url: "/register",
+        method: "post",
+        data,
+    })
+}
